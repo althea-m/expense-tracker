66738fbd312c383e637af9c8fe6b24561110c2a0
althyy <161809177+althyy@users.noreply.github.com>
2026-03-17 20:26:10 +1100

    interfacing

--- expense-tracking/src/App.jsx
+++ expense-tracking/src/App.jsx
@@ -1,120 +1,93 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const data = Object.fromEntries(formData.entries())
+    console.log(data)
+  }
+
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+    <div>
+        <div className="header">
+          <h1>Expense Tracker</h1>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+        <div className="topbar">
+          <div className="total-expenses">
+            <section className="card">
+              <h2>Total Expenses</h2>
+            </section>
+          </div>
+          <div className="top-categories">
+            <section className="card">
+              <h2>Top Category</h2>
+            </section>
+          </div>  
+          <div className="transaction-number">
+            <section className="card">
+              <h2>Number of Transactions</h2>
+            </section>
+          </div>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
         </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
+        <div className="container">
+          <div className="container-left">
+            <section className="card">
+              <h2>Add Transaction</h2>
+                <form id="transaction-form" onSubmit={handleSubmit}>
+                  <div className="form-row">
+                  <label>
+                    Title:
+                  </label>
+                    <input type="text" name="title" required />
+                  </div>
+                  <div className="form-row">
+                  <label>
+                    Amount:
+                  </label>
+                    <input type="number" name="amount" required />
+                  </div>
+                  <div className="form-row">
+                  <label>
+                    Category:
+                  </label>
+                    <input type="text" name="category" required />
+                  </div>
+                  <div className="form-row">
+                  <label>
+                    Date:
+                  </label>
+                    <input type="date" name="date" required />
+                  </div>
+                  <div className="form-row">
+                  <label>
+                    Description:
+                  </label>
+                    <textarea name="description" rows="3"></textarea>
+                  </div>
+                  <button id="add-transaction-btn" type="submit">Add</button>
+                </form>
+            </section>
+            <section className="card">
+              <h2>Recent Transactions</h2>
+            </section>
+          </div>
+          
+          <div className="container-right">
+            <section className="card">
+              <h2>Monthly Summary</h2>
+              
+            </section>
+            <section className="card">
+              <h2>Category Breakdown</h2>
+            </section>
+          </div>
         </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    </div>
   )
 }
 
